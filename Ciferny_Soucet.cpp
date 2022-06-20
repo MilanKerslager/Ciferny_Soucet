cadb2b33303122c5367719ef24468d8aa14e9901
Milan Keršláger <milan.kerslager@pslib.cz>
2022-06-20 13:40:04 +0200

    Přidejte soubory projektu.

---
+++ Ciferny_Soucet.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int cislo = 123;
+	int soucet = 0;
+
+	printf("Ciferny soucet cisla %d", cislo);
+	for (; cislo > 0; soucet += cislo % 10, cislo /= 10);
+	printf(" je %d\n", soucet);
+}
